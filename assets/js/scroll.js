fe54f6b0906a8ec605aef03e964255def19fe190
evgeniiDJS <hronologia2019@gmail.com>
2021-09-19 23:15:08 +0300

    fix: script

--- assets/js/scroll.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("a[href^='#']").click(function(){
-            var _href = $(this).attr("href");
-            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-            return false;
-    });
-});
