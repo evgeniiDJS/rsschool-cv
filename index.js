ebf8c1b3b7e5de2c568ac86c9fb772d400a8e3c7
evgeniiDJS <hronologia2019@gmail.com>
2021-11-25 18:51:31 +0400

    fat: adaptive

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 hljs.initHighlightingOnLoad();
+
+const menu__btn = document.querySelector('.menu__btn');
+const container = document.querySelector('.container');
+const menu__toggle = document.querySelector('#menu__toggle')
+
+menu__btn.addEventListener('click', () => {
+    
+})
+
+document.querySelector('#menu__toggle').addEventListener('change', function(e) {
+    document.body.style.overflow = e.target.checked === true ? 'hidden' : '';
+});
